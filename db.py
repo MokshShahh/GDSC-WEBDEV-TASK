12b6ec327acf695629227f779ecbf7682f586738
Moksh <moksh13112006@gmail.com>
2024-08-02 00:50:13 +0530

    database refactored

--- db.py
+++ db.py
@@ -1,13 +1,15 @@
-# import sqlite3
-# con = sqlite3.connect("data.db")
-# cur = con.cursor()
-# cur.execute("delete from music")
-# cur.execute("CREATE TABLE IF NOT EXISTS music(song, artist)")
-# # cur.execute("""INSERT INTO music(song,artist) 
-# #             VALUES
-# #             ('In my room','Frank Ocean'),
-# #             ('The less i know the better','Tame Impala'),
-# #             ('Chamber of reflection','Mac Demarco')""")
-# # cur.execute("commit")
+#used these commands to make and fill my db with dummy content so that get requests has something to show
 
-# print(cur.execute("select * from music").fetchall())
+import sqlite3
+con = sqlite3.connect("data.db")
+cur = con.cursor()
+cur.execute("delete from music")
+cur.execute("CREATE TABLE IF NOT EXISTS music(song, artist)")
+cur.execute("""INSERT INTO music(song,artist) 
+            VALUES
+            ('In my room','Frank Ocean'),
+            ('The less i know the better','Tame Impala'),
+            ('Chamber of reflection','Mac Demarco')""")
+cur.execute("commit")
+
+print(cur.execute("select * from music").fetchall())
