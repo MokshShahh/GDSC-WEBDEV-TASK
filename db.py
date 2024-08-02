0b2bb1d3f0faa73beac6a3f40c719373c4c4d32e
Moksh <moksh13112006@gmail.com>
2024-08-02 17:24:58 +0530

    updated documentation

--- db.py
+++ db.py
@@ -4,20 +4,17 @@ import sqlite3
 con = sqlite3.connect("data.db")
 cur = con.cursor()
 cur.execute("delete from music")
+
+#creating schema of the sqlite3 table
 cur.execute("CREATE TABLE IF NOT EXISTS music(song, artist)")
+
+#putting in dummy data
 cur.execute("""INSERT INTO music(song,artist) 
             VALUES
             ('In my room','Frank Ocean'),
             ('The less i know the better','Tame Impala'),
             ('Chamber of reflection','Mac Demarco')""")
-song="In my room"
 
-cur.execute("SELECT 1 from music where song=?",(song,))
-result=cur.fetchone()
-if result:
-    cur.execute("DELETE FROM music where song=?",(song,))
-else:
-    print("song did not exist in table")
 
 cur.execute("commit")
 
